dfa781768e5d6130fc8b544a7e406e5da7ccf360
Sarah Schwartz <sarah.schwartz93@gmail.com>
2022-05-09 10:54:47 -0700

    formated

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,18 +1,18 @@
-import {Address } from "@graphprotocol/graph-ts"
+import { Address } from "@graphprotocol/graph-ts";
 import {
   ConfirmedAttendee,
   NewEventCreated,
   NewRSVP,
-  DepositsPaidOut
-} from "../generated/Web3RSVP/Web3RSVP"
-import { Account, RSVP, Confirmation, Event } from "../generated/schema"
+  DepositsPaidOut,
+} from "../generated/Web3RSVP/Web3RSVP";
+import { Account, RSVP, Confirmation, Event } from "../generated/schema";
 
 export function handleNewEventCreated(event: NewEventCreated): void {
   let newEvent = Event.load(event.params.eventID.toHex());
   if (newEvent == null) {
     newEvent = new Event(event.params.eventID.toHex());
     newEvent.eventOwner = event.params.creatorAddress.toString();
-    newEvent.eventTimestamp = event.params.eventTimestamp
+    newEvent.eventTimestamp = event.params.eventTimestamp;
     newEvent.maxCapacity = event.params.maxCapacity;
     newEvent.deposit = event.params.deposit;
     newEvent.paidOut = false;
@@ -22,7 +22,7 @@ export function handleNewEventCreated(event: NewEventCreated): void {
 
 function getOrCreateAccount(address: Address): Account {
   let account = Account.load(address.toHex());
-  if (account == null){
+  if (account == null) {
     account = new Account(address.toHex());
   }
   return account;
@@ -32,11 +32,11 @@ export function handleNewRSVP(event: NewRSVP): void {
   let newRSVP = RSVP.load(event.transaction.from.toHex());
   let account = getOrCreateAccount(event.params.attendeeAddress);
   let thisEvent = Event.load(event.params.eventID.toHex());
-  if (newRSVP == null && thisEvent != null){
+  if (newRSVP == null && thisEvent != null) {
     newRSVP = new RSVP(event.transaction.from.toHex());
     newRSVP.attendee = account.id;
     newRSVP.event = thisEvent.id;
-    newRSVP.save()
+    newRSVP.save();
   }
 }
 
@@ -44,19 +44,19 @@ export function handleConfirmedAttendee(event: ConfirmedAttendee): void {
   let newConfirmation = Confirmation.load(event.transaction.from.toHex());
   let account = getOrCreateAccount(event.params.attendeeAddress);
   let thisEvent = Event.load(event.params.eventID.toHex());
-  if (newConfirmation == null && thisEvent != null){
+  if (newConfirmation == null && thisEvent != null) {
     newConfirmation = new Confirmation(event.transaction.from.toHex());
     newConfirmation.attendee = account.id;
     newConfirmation.event = thisEvent.id;
-    newConfirmation.save()
+    newConfirmation.save();
   }
 }
 
 export function handleDepositsPaidOut(event: DepositsPaidOut): void {
   let thisEvent = Event.load(event.params.eventID.toHex());
-  if(thisEvent){
+  if (thisEvent) {
     thisEvent.paidOut = true;
-  } else{
-    console.log("OH NO")
+  } else {
+    console.log("OH NO");
   }
 }
