165f9a3f46afe43466f892989fc2c81d8f55ba16
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2022-05-09 11:03:23 -0700

    save account in getOrCreateAccount

--- src/mapping.ts
+++ src/mapping.ts
@@ -24,6 +24,7 @@ function getOrCreateAccount(address: Address): Account {
   let account = Account.load(address.toHex());
   if (account == null) {
     account = new Account(address.toHex());
+    account.save();
   }
   return account;
 }
